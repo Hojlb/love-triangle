70dfc9ce15d9d6735cf469af5ec5da2f141847a6
Maksim Palonski <mchp010nsky@gmail.com>
2018-02-20 20:40:04 +0300

    solver

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    // your implementation
+        let count = 0;
+        for(let i = 0; i < preferences.length; i++){
+            let a = i+1;
+            let b = preferences[ a - 1 ];
+            let c = preferences[ b - 1 ];
+            let d = preferences[ c - 1 ];
+
+        if( d===a && b !==c ) count ++;
+        };
+
+        return count/3;
+
+    };
